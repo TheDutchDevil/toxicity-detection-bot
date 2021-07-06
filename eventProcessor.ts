3929a91ae6eb0bb0e6744341ff313a32b12d3e92
Nathan Cassee <n.w.cassee@tue.nl>
2021-07-06 10:51:59 +0200

    Update threshold

--- eventProcessor.ts
+++ eventProcessor.ts
@@ -134,7 +134,7 @@ export enum Triggers {
 
             core.info(`Processing Toxicity Command of location: ${command.location} and trigger ${command.trigger}`);
             
-            const detector = new ToxicityDetector(0.7);
+            const detector = new ToxicityDetector(0.6);
 
             await detector.processCommand(<ToxicityCommand>command);
         } 
